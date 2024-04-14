9faf028771b0645c52112a8cef75230d865db21a
Richard Zianne <5277138+RachidZianne@users.noreply.github.com>
2024-04-14 17:49:03 +0200

    Update Dart file

--- lib/constants/app_config.dart
+++ lib/constants/app_config.dart
@@ -2,10 +2,10 @@ class AppConfig {
   static final ECOMMERCE_URL        = "https://abc123.winplazza.com";
   // static final ECOMMERCE_URL        = "https://rawal-dev.vector-coder.com";
 
-  static const CONSUMER_KEY           = "{"id":50,"key":"client_id","value":"1234","type":"website_general","created_by":null,"updated_by":null,"created_at":null,"updated_at":null}";
-  static const CONSUMER_SECRET        = "{"id":49,"key":"client_secret","value":"sk_1234","type":"website_general","created_by":null,"updated_by":null,"created_at":null,"updated_at":null}";
-  static const ONESIGNAL_APP_ID       = "{"id":130,"key":"onesignal_app_id","value":"6053d948-b8f6-472a-87e4-379fa89f78d8","type":"business_notification_setting","created_by":null,"updated_by":null,"created_at":null,"updated_at":null}";
-  static const PLACE_PICKER_API_KEY   = "{"id":177,"key":"google_map_api_string","value":"","type":"business_google_setting","created_by":null,"updated_by":null,"created_at":null,"updated_at":null}";
+  static const CONSUMER_KEY           = "TESTO1234";
+  static const CONSUMER_SECRET        = "testoSk_1234";
+  static const ONESIGNAL_APP_ID       = "6053d948-b8f6-472a-87e4-379fa89f78d8";
+  static const PLACE_PICKER_API_KEY   = "ELTESTO54725475275275472554275";
 
   static const bool IS_DEMO_ENABlED   = false;
   static const int APP_DESIGN = 1;
